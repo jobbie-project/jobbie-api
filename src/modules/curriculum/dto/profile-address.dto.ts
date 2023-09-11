42498e0977aec185085631544759982bf71423bc
Adrielly Isly <114019643+islycodes@users.noreply.github.com>
2023-09-11 20:14:09 -0300

    update profile-address.dto

--- src/modules/curriculum/dto/profile-address.dto.ts
+++ src/modules/curriculum/dto/profile-address.dto.ts
@@ -1,6 +1,53 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class ProfileAddress {
+  @IsNotEmpty({
+    context: {
+      message: "missing-street",
+      userMessage: "Rua obrigatória",
+    },
+  })
+  @IsString({
+    context: {
+      message: "invalid-street",
+      userMessage: "Rua inválida",
+    },
+  })
   street: string;
+
+  @IsNotEmpty({
+    context: {
+      message: "missing-city",
+      userMessage: "Cidade obrigatória",
+    },
+  })
+  @IsString({
+    context: {
+      message: "invalid-city",
+      userMessage: "Cidade inválida",
+    },
+  })
   city: string;
+
+  @IsNotEmpty({
+    context: {
+      message: "missing-state",
+      userMessage: "Estado obrigatório",
+    },
+  })
   state: string;
+
+  @IsNotEmpty({
+    context: {
+      message: "missing-zip-code",
+      userMessage: "CEP obrigatório",
+    },
+  })
+  @IsString({
+    context: {
+      message: "invalid-zip-code",
+      userMessage: "CEP inválido",
+    },
+  })
   zip_code: string;
 }
