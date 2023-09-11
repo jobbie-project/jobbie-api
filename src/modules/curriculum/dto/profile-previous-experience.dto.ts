4769d9ce47b4dd896e0c212fd8cad3bda44e20f3
Felipe Gabriel Botelho <felipeg.bot@gmail.com>
2023-09-11 20:14:58 -0300

    Update profile-previous-experience.dto.ts

--- src/modules/curriculum/dto/profile-previous-experience.dto.ts
+++ src/modules/curriculum/dto/profile-previous-experience.dto.ts
@@ -1,11 +1,75 @@
+import { IsDateString, IsNotEmpty, IsOptional, IsString, ValidateNested } from "class-validator";
+import { LocationDto } from "./location.dto";
+import { Type } from "class-transformer";
+
 export class ProfilePreviousExperience {
+  @IsNotEmpty({
+    context: {
+      message: "missing-company-name",
+      userMessage: "Nome da empresa obrigatório",
+    },
+  })
+  @IsString({
+    context: {
+      message: "invalid-company-name",
+      userMessage: "Nome da empresa inválido",
+    },
+  })
   company_name: string;
+
+  @IsNotEmpty({
+    context: {
+      message: "missing-position",
+      userMessage: "Cargo obrigatório",
+    },
+  })
+  @IsString({
+    context: {
+      message: "invalid-position",
+      userMessage: "Cargo inválido",
+    },
+  })
   position: string;
+
+  @IsNotEmpty({
+    context: {
+      message: "missing-start-date",
+      userMessage: "Data de início obrigatória",
+    },
+  })
+  @IsDateString(
+    {},
+    {
+      context: {
+        message: "invalid-start-date",
+        userMessage: "Data de início inválida",
+      },
+    }
+  )
   start_date: Date;
+
+  @IsOptional()
+  @IsDateString(
+    {},
+    {
+      context: {
+        message: "invalid-end-date",
+        userMessage: "Data de término inválida",
+      },
+    }
+  )
   end_date?: Date;
-  location: {
-    city: string;
-    state: string;
-  };
-  description: string;
+
+  @ValidateNested()
+  @Type(() => LocationDto)
+  location: LocationDto;
+
+  @IsOptional()
+  @IsString({
+    context: {
+      message: "invalid-description",
+      userMessage: "Descrição inválida",
+    },
+  })
+  description?: string;
 }
