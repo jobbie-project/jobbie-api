e3daf38e61a9668e1f23d65c63f5d32e9991d9d9
Felipe Gabriel Botelho <felipeg.bot@gmail.com>
2023-11-19 03:39:25 -0300

    implements docker script file

---
+++ script-docker.sh
@@ -0,0 +1,12 @@
+# Cria a rede Docker
+docker network create jobbie-net
+
+# Cria o serviço postgres
+docker run --name=postgres --net jobbie-net -e POSTGRES_PASSWORD=lipao2002 -e POSTGRES_DB=jobbie -e POSTGRES_USER=postgres -p 5440:5432 -d postgres:latest
+
+# Executa a API
+docker run --name=jobbie-api --net jobbie-net --env-file .env -p 1337:1337 -d jobbie-api:1.0
+
+# Executa o aplicativo
+docker run --name=jobbie-app --net jobbie-net -e VITE_BACKEND_URL=jobbie-api:1337 -p 3000:3000 -d jobbie-app:1.0
+
